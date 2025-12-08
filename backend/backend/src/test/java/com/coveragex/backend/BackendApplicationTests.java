662565d4278d0eea05e601f4518869ff009524b8
SandaruwanWeerawardhana <werawardanasw@gmail.com>
2025-12-08 22:17:56 +0530

    chore: initialize project setup and commit initial version to repository

---
+++ backend/backend/src/test/java/com/coveragex/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coveragex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
